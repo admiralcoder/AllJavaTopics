c8549083801583a70b6a8cbd5ed77235b8a1570d
Alex Rodriguez <djale2008@gmail.com>
2018-11-26 21:47:45 -0500

    Adding Array examples

--- src/Example_arrays/Arrays_app.java
+++ src/Example_arrays/Arrays_app.java
@@ -4,7 +4,7 @@ public class Arrays_app {
 
     public static void main(String[] args) {
         //what would be the output of this Scenario.
-        String [] stringNames = {"alex", "Melisa", "rodriguez"};
+        String [] stringNames = {"Smith", "Michell", "Roger"};
         for (int i=0; i<=3; i++){
             System.out.println(stringNames[i]);
         }
