2a1aa7544dc0cec3800f89b1360d178873f8558a
Alex Rodriguez <djale2008@gmail.com>
2018-11-26 22:45:23 -0500

    update if Statement

--- src/If_else_Statement/InterestRate.java
+++ src/If_else_Statement/InterestRate.java
@@ -1,6 +1,22 @@
 package If_else_Statement;
 
-public class BankStatement {
+public class InterestRate {
 
-    main
+    //method to print simple InterestRate using if else statements
+    public static final double Overdrawn_Penalty = 8.00;
+    public static final double interestRate = 0.5;
+
+    public static void main(String[] args) {
+
+        bankInterest(20);
+
+    }
+
+
+    public static void bankInterest(double balance) {
+        if (balance >= 0) {
+            balance = balance + (interestRate * balance) / 12;
+        } else balance = balance - Overdrawn_Penalty;
+        System.out.println("new balance is $" + balance);
+    }
 }
--- src/If_else_Statement/SameWord.java
+++ src/If_else_Statement/SameWord.java
@@ -1,4 +1,23 @@
 package If_else_Statement;
 
 public class SameWord {
+
+    public static void main(String... args) {
+
+        /*
+        Write a method that will print matching words that will ignore the case
+        Ex: alexandria, Alexandria = match
+        Ex: alexandria, Miami = doesnt match
+         */
+
+        word("alexandria", "Alexandria");
+    }
+
+    public static void word(String first, String second) {
+        if (first.equalsIgnoreCase(second)) {
+            System.out.println("your first word is " + first + " and it matches second word " + second);
+        } else {
+            System.out.println("your word doesnt match " + first + " , " + second);
+        }
+    }
 }
